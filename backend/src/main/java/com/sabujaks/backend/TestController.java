a021be2b143c18606d3ac15d1045a5e1b3dac1a4
mpqm <okqkrwhdtjd@gmail.com>
2024-08-27 11:33:46 +0900

    [Init] 프로젝트 브랜치 설정

--- backend/src/main/java/com/sabujaks/backend/TestController.java
+++ backend/src/main/java/com/sabujaks/backend/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("test1");
+        return ResponseEntity.ok("test2");
     }
 }
