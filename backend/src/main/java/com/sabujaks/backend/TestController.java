6b9f779a8405d2e0440c5bb29606f03616bf0640
seo-jae-eun <wodms5522@naver.com>
2024-08-28 15:17:15 +0900

    [Test] Push Test

--- backend/src/main/java/com/sabujaks/backend/TestController.java
+++ backend/src/main/java/com/sabujaks/backend/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("v1.105\n");
+        return ResponseEntity.ok("v1.106\n");
     }
 }
